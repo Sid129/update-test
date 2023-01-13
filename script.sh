acf1a6b3c351dcf8c789ad7192af769af3895b21
Sid129 <71804669+Sid129@users.noreply.github.com>
2023-01-13 18:53:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,9 +5,9 @@ if git diff --quiet; then
     echo "targetRevision was not changed"
 else
     echo "targetRevision was changed"
-git checkout -b updated_targetrevision
-git commit -m "Update targetRevision in values.yml"
-git push origin updated_targetrevision_values_file
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d '{"title":"Updated targetRevision in values.yml","head":"updated_targetrevision","base":"main"}' https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/pulls
+    git checkout -b updated_targetrevision
+    git commit -m "Update targetRevision in values.yml"
+    git push origin updated_targetrevision_values_file
+    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d '{"title":"Updated targetRevision in values.yml","head":"updated_targetrevision","base":"main"}' https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/pulls
 fi 
 
