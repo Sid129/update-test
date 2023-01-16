2cdeab54827fffcfc579c1287cf512c22e1693dc
Sid129 <71804669+Sid129@users.noreply.github.com>
2023-01-16 17:24:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,14 @@
-git config --global user.email "sachinuppar129@gmail.com"
-git config --global user.name "Sidd"
-sudo yq -i ".landscapes.add='new'" ./values.yml
-if git diff --quiet; then
-    echo "targetRevision was not changed"
-else
-    echo "targetRevision was changed"
-    git checkout -b updated_targetrevision
-    git commit -m "Update targetRevision in values.yml"
-    git push origin updated_targetrevision_values_file
-    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d '{"title":"Updated targetRevision in values.yml","head":"updated_targetrevision","base":"main"}' https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/pulls
-fi 
+ls
+# git config --global user.email "sachinuppar129@gmail.com"
+# git config --global user.name "Sidd"
+# sudo yq -i ".landscapes.add='new'" ./values.yml
+# if git diff --quiet; then
+#     echo "targetRevision was not changed"
+# else
+#     echo "targetRevision was changed"
+#     git checkout -b updated_targetrevision
+#     git commit -m "Update targetRevision in values.yml"
+#     git push origin updated_targetrevision_values_file
+#     curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d '{"title":"Updated targetRevision in values.yml","head":"updated_targetrevision","base":"main"}' https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/pulls
+# fi 
 
